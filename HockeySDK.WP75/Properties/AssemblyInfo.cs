137c37b31330084ba6fa7aca2f5087c4924597c7
Davide Mendolia <davide@tuenti.com>
2013-09-18 12:37:58 +0200

    Initial support for nuget

--- HockeySDK.WP75/Properties/AssemblyInfo.cs
+++ HockeySDK.WP75/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HockeySDK.WP75")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows Phone Class Library to do crash reporting with hockeyapp.net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HockeySDK.WP75")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Codenauts UG")]
+[assembly: AssemblyProduct("HockeySDK")]
+[assembly: AssemblyCopyright("Copyright © Codenauts UG 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
