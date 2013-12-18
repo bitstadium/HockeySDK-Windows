ade9f1955849b104fc8091bb574fb77fcf998153
Sandy Armstrong <sanfordarmstrong@gmail.com>
2013-12-18 07:41:02 -0800

    Fix URL for README that is opened after nuget install
    This should be updated again if the master branch ever
    becomes the desired place to look for the README.

--- Nuget/tools/install.ps1
+++ Nuget/tools/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-$project.DTE.ItemOperations.Navigate('http://github.com/bitstadium/HockeySDK-Windows/readme.txt')
+$project.DTE.ItemOperations.Navigate('https://github.com/bitstadium/HockeySDK-Windows/blob/develop/README.md')
 
 //TODO braucht es das oder ist content der default ?! funzt das mit Extension (orig: Name)
 $item = $project.ProjectItems | where-object {$_.Extension -eq "png"} 
