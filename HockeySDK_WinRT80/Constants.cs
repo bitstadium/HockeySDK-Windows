d984d006393e191688d7f5ba9b80162e880b1b97
Andreas Moosbrugger <am@mogular.com>
2014-06-27 12:44:23 +0200

    support for windows 8

---
+++ HockeySDK_WinRT80/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HockeyApp
+{
+    internal class Constants
+    {
+
+        internal const string CRASHPATHNAME = "HockeyCrashes";
+
+        internal const string CrashFilePrefix = "crashinfo_";
+
+        internal const string USER_AGENT_STRING = "Hockey/WinRT80";
+        internal const string SDKNAME = "HockeySDKWinRT80";
+        internal const string SDKVERSION = "2.0.4";
+    }
+}
