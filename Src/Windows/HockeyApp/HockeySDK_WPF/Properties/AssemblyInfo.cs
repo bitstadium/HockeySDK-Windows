c608e64ecae9e493b2dd34f3c6841af38abfe490
Mike Smacinih <mihailsm@microsoft.com>
2015-10-01 10:13:50 -0700

    Integrate HockeySDK_WPF Nuget into the build system.

--- Src/Windows/HockeyApp/HockeySDK_WPF/Properties/AssemblyInfo.cs
+++ Src/Windows/HockeyApp/HockeySDK_WPF/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ba0531b9-acad-4c8e-8b40-8bc4193a62ed")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.2.0")]
-[assembly: AssemblyFileVersion("2.2.2.0")]
