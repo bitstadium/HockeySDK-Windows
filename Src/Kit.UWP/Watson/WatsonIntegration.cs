3e38d81b4e36246459e3276d6181970cad1c48ce
Mike Smacinih <mihailsm@microsoft.com>
2016-01-27 13:47:33 -0800

    Watson integration

---
+++ Src/Kit.UWP/Watson/WatsonIntegration.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.HockeyApp.Watson
+{
+    internal class WatsonIntegration
+    {
+        internal static void Integrate(string hockeyAppIKey)
+        {
+            using (var eventSource = new Microsoft.Diagnostics.Telemetry2.PartnerTelemetryEventSource("HockeyAppEventSource"))
+            {
+                eventSource.Write("HockeyAppWatsonIdentification", new { PartA_iKey = hockeyAppIKey });
+            }
+        }
+    }
+}
