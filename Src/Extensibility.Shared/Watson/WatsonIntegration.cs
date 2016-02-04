effd104732f490bd847f46788d32718402e5c399
Mike Smacinih <mihailsm@microsoft.com>
2016-02-04 10:01:22 -0800

    Adding HockeyAppCorrelationGuid to UTC event.

--- Src/Extensibility.Shared/Watson/WatsonIntegration.cs
+++ Src/Extensibility.Shared/Watson/WatsonIntegration.cs
@@ -1,5 +1,6 @@
 ﻿namespace Microsoft.HockeyApp.Watson
 {
+    using System;
     using System.Diagnostics.Tracing;
 
     internal class WatsonIntegration
@@ -8,7 +9,7 @@
         {
             using (var eventSource = new PartnerTelemetryEventSource("Microsoft.HockeyApp.HockeyAppEventSource"))
             {
-                eventSource.Write("HockeyAppWatsonIdentification", new HockeyAppIdentity() { PartA_iKey = hockeyAppIKey });
+                eventSource.Write("HockeyAppWatsonIdentification", new HockeyAppIdentity() { PartA_iKey = hockeyAppIKey, HockeyAppCorrelationGuid = Guid.NewGuid().ToString() });
             }
         }
 
@@ -16,6 +17,12 @@
         internal class HockeyAppIdentity
         {
             internal string PartA_iKey { get; set; }
+
+            /// <summary>
+            /// Gets or sets a GUID in HockeyApp UTC event as well.
+            /// This GUID will permit Watson team to join, in the future, if they end up posting directly to Watson from inside the UWP app.
+            /// </summary>
+            internal string HockeyAppCorrelationGuid { get; set; }
         }
     }
 }
