0638c89803fefe1eba6409a9e2d1c03c46add516
Mike Smacinih <mihailsm@microsoft.com>
2016-05-09 14:58:56 -0700

    Partially remove Portable40

---
+++ Src/Kit.Core45/Properties/AssemblyInfoCore.cs
@@ -0,0 +1,18 @@
+﻿internal static class AssemblyInfo
+{
+#if DEBUG
+    // Internal key; assemblies are fully signed.
+    public const string PublicKey = "";
+    public const string MoqPublicKey = "";
+    public const string SystemRuntimeSerializationPublicKey = "";
+#else
+    // Public key; assemblies are delay signed.
+    public const string PublicKey = ", PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293";
+    public const string MoqPublicKey = ", PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7";
+
+    /// <summary>
+    /// Public key postfix for InternalsVisibleToAttribute for System.Runtime.Serialization assembly.
+    /// </summary>
+    public const string SystemRuntimeSerializationPublicKey = ", PublicKey=00240000048000009400000006020000002400005253413100040000010001008D56C76F9E8649383049F383C44BE0EC204181822A6C31CF5EB7EF486944D032188EA1D3920763712CCB12D75FB77E9811149E6148E5D32FBAAB37611C1878DDC19E20EF135D0CB2CFF2BFEC3D115810C3D9069638FE4BE215DBF795861920E5AB6F7DB2E2CEEF136AC23D5DD2BF031700AEC232F6C6B1C785B4305C123B37AB";
+#endif
+}
