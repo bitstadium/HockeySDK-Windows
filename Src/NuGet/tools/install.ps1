7ffa68a37c2d501831bcc3ba5f3400777adfb51b
Mike Smacinih <mihailsm@microsoft.com>
2016-06-02 13:06:49 -0700

    Fix link to preseason README.md

--- Src/NuGet/tools/install.ps1
+++ Src/NuGet/tools/install.ps1
@@ -1,3 +1,3 @@
 param($installPath, $toolsPath, $package, $project)
 
-$project.DTE.ItemOperations.Navigate('https://github.com/bitstadium/HockeySDK-Windows-Private/blob/master/README.md')
+$project.DTE.ItemOperations.Navigate('https://github.com/bitstadium/HockeySDK-Windows/blob/preseason/README.md')
