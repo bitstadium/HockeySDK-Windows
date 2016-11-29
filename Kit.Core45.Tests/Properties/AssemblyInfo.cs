f898481322d5a4dd1f85ec0ade78565ad6de3a8b
Izik Lisbon <izikl@microsoft.com>
2016-11-28 18:02:03 -0800

    Add unit tests to the Core45 project

---
+++ Kit.Core45.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kit.Core45.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Kit.Core45.Tests")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform", "Core45")]
